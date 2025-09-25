4bc4e650f4e10101ff32d926b8af6eb1040d00e2
Alejandro <xenuti@gmail.com>
2025-09-25 16:18:37 -0600

    esbozo de app separador

---
+++ apps/app_escritorio_ultimate_separador/interfaz_ultimate_separador.py
@@ -0,0 +1,69 @@
+import PyPDF2
+import re
+import os
+import zipfile
+
+import io
+
+# Función para extraer el titular de la cuenta de una página
+def extract_titular(text):
+    match = re.search(r'SA DE CV  Titular de la cuenta:\s*([A-Z\s]+)', text)
+    if match:
+        return match.group(1).strip()
+    match = re.search(r'\nCREDITO  Titular de la cuenta:\s*([A-Z\s]+)', text)
+    if match:
+        return match.group(1).strip()
+    match = re.search(r'\nCV  Titular de la cuenta:\s*([A-Z\s]+)', text)
+    if match:
+        return match.group(1).strip()
+
+
+
+def separar_paginas ( pdf_path : str, output_dir : str, fecha : str ) -> list[io.BytesIO]:
+    # Leer el PDF y separar por páginas
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+
+        pdf_en_memoria_buffer = []
+
+        for i, page in enumerate(reader.pages):
+            text = page.extract_text()
+            titular = extract_titular(text)
+            if not titular:
+                titular = f"Desconocido_{i+1}"
+            else:
+                titular = '_'.join(titular.split())
+
+            output_filename = f"{fecha}_{titular}_{i+1}.pdf"
+            output_path = os.path.join(output_dir, output_filename)
+
+            #creamos un elemento en memoria (buffer)
+            output_buffer = io.BytesIO()
+
+            # Crear nuevo PDF con la página extraída
+            writer = PyPDF2.PdfWriter()
+            writer.add_page(page)
+
+            writer.write( output_buffer )
+
+            output_buffer.seek(0) 
+            pdf_en_memoria_buffer.append( (output_buffer, output_filename) )
+
+            return pdf_en_memoria_buffer
+        
+
+def funcion_principal( pdf_path : str, output_dir : str, fecha : str, numero_archivo : int ) -> None:
+    
+    lista_pdf_buffer = separar_paginas(pdf_path= pdf_path, output_dir= output_dir, fecha=fecha )
+
+    # Comprimir todos los PDFs generados
+    zip_filename = f"/content/PROVEEDORES_{fecha}_{numero_archivo}.zip"
+    with zipfile.ZipFile(zip_filename, "w") as zipf:
+        for root, _, files_in_dir in os.walk(output_dir):
+            for file in files_in_dir:
+                zipf.write(os.path.join(root, file), arcname=file)
+
+
+    # Descargar el archivo ZIP
+    #files.download(zip_filename)
+
